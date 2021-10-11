cd7ae1cf204990baacd91c95311c8be715e10dad
AlexandreFontainee <71234514+AlexandreFontainee@users.noreply.github.com>
2021-10-11 02:55:22 +0200

    first commit
    fetch de l'api

---
+++ front/html/script.js
@@ -0,0 +1,13 @@
+function fetchBaseProduit(){
+
+    fetch("http://localhost:3000/api/products")
+    .then(response => response.json())
+    .then((data)=> {
+        console.log(data);
+    })
+}
+fetchBaseProduit();
+
+
+
+document.getElementById('items')
