2726e51365a627cea0152af59ca66ec1819eafe8
AlexandreFontainee <71234514+AlexandreFontainee@users.noreply.github.com>
2021-10-12 06:23:05 +0200

    Update script.js

--- front/html/script.js
+++ front/html/script.js
@@ -1,13 +1,37 @@
-function fetchBaseProduit(){
+// appel de l'api 
 
-    fetch("http://localhost:3000/api/products")
+fetch("http://localhost:3000/api/products")
     .then(response => response.json())
     .then((data)=> {
         console.log(data);
-    })
+
+
+// variable pour l'affichage du html
+let affichage ="";
+
+
+// boucle pour avoir les éléments du tableau 
+for(let i = 0; i < data.length; i++) {
+
+    affichage += `<section id="items">
+
+    <a href="./product.html?${data[i]._id}">
+            <article>
+              <img src="${data[i].imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
+              <h3 class="productName">${data[i].name}</h3>
+              <p class="productDescription">${data[i].description}</p>
+            </article>
+          </a> `
+
 }
-fetchBaseProduit();
 
+document.getElementById("items").innerHTML = affichage
 
+})
 
-document.getElementById('items')
+// message d'erreur
+.catch(e => {
+    errorMessage();
+    console.log(e);
+});
+
