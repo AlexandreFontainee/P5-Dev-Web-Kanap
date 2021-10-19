600679201dc1d38aed68a434f9448a8f59ad7e93
AlexandreFontainee <71234514+AlexandreFontainee@users.noreply.github.com>
2021-10-19 05:47:45 +0200

    second commit
    product page,
    new fetch method

---
+++ front/js/product.js
@@ -0,0 +1,40 @@
+let resultApi;
+const image = document.querySelector('.item__img');
+const titre = document.querySelector('#title');
+const prix = document.querySelector('#price');
+const description = document.querySelector('#description');
+let colorSelect = document.getElementById("colors");
+
+
+// appel de l'api 
+fetch(`http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926`)
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data);
+
+    resultApi = data;
+
+    titre.innerText = resultApi.name;
+    prix.innerText = resultApi.price;
+    image.innerHTML = `<img src="${resultApi.imageUrl}" alt="Photographie d'un canapé">`
+    description.innerText = resultApi.description;
+
+
+    for (let i = 0; i < resultApi.colors.length; i++) {
+      let option = document.createElement("option");
+      option.innerText = resultApi.colors[i];
+      colorSelect.appendChild(option);
+
+    };
+
+  })
+  // message d'erreur
+  .catch(e => {
+    errorMessage("Veuillez passer sur le localhost:3000");
+    console.log(e);
+  });
+
+  
+
+
+
---
+++ front/js/script.js
@@ -0,0 +1,38 @@
+// appel de l'api 
+
+  fetch(`http://localhost:3000/api/products/`)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+
+
+      // variable pour l'affichage du html
+      let affichage = "";
+
+
+      // boucle pour avoir les éléments du tableau 
+      for (let i = 0; i < data.length; i++) {
+
+        affichage += `
+
+    <a href="./product.html?${data[i]._id}">
+            <article>
+              <img src="${data[i].imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
+              <h3 class="productName">${data[i].name}</h3>
+              <p class="productDescription">${data[i].description}</p>
+            </article>
+          </a> `;
+
+      }
+
+      document.getElementById("items").innerHTML = affichage;
+
+    })
+
+    // message d'erreur
+    .catch(e => {
+      errorMessage("Veuillez passer sur le localhost:3000");
+      console.log(e);
+    });
+
+  
