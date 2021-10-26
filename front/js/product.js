69a74d353dfd111c832bf055f9b049cadb6b4221
AlexandreFontainee <71234514+AlexandreFontainee@users.noreply.github.com>
2021-10-27 00:00:45 +0200

    localStorage
    mise en place du local storage

--- front/js/product.js
+++ front/js/product.js
@@ -1,42 +1,83 @@
+// utilisation de searchparams pour récupérer l'id du bon produit pour chaque objet 
+const str = window.location;
+const url = new URL(str);
+const id = url.searchParams.get("id");
+const newUrl = "http://localhost:3000/api/products/" + id;
+
 let resultApi;
 const image = document.querySelector('.item__img');
 const titre = document.querySelector('#title');
 const prix = document.querySelector('#price');
 const description = document.querySelector('#description');
 let colorSelect = document.getElementById("colors");
+const itemQty = document.getElementById("quantity");
 
 
 // appel de l'api 
-fetch(`http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926`)
-  .then(response => response.json())
-  .then((data) => {
-    console.log(data);
+function fetchNewUrl() {
 
-    resultApi = data;
+  fetch(newUrl)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
 
-    // mise en page de l'api avec le DOM
-    titre.innerText = resultApi.name;
-    prix.innerText = resultApi.price;
-    image.innerHTML = `<img src="${resultApi.imageUrl}" alt="Photographie d'un canapé">`
-    description.innerText = resultApi.description;
+      resultApi = data;
+
+      // mise en page de l'api avec le DOM
+      titre.innerText = resultApi.name;
+      prix.innerText = resultApi.price;
+      image.innerHTML = `<img src="${resultApi.imageUrl}" alt="Photographie d'un canapé">`
+      description.innerText = resultApi.description;
 
 
-    // boucle pour mettre en place les options de couleurs 
-    for (let i = 0; i < resultApi.colors.length; i++) {
-      let option = document.createElement("option");
-      option.innerText = resultApi.colors[i];
-      colorSelect.appendChild(option);
+      // boucle pour mettre en place les options de couleurs 
+      for (let i = 0; i < resultApi.colors.length; i++) {
+        let option = document.createElement("option");
+        option.innerText = resultApi.colors[i];
+        colorSelect.appendChild(option);
 
+      };
+
+    });
+
+};
+fetchNewUrl();
+
+
+function addToCart() {
+
+  // on crée le formulaire d'envoie du bouton 
+  const sendToCart = document.getElementById("addToCart");
+  sendToCart.addEventListener('click', (event) => {
+    event.preventDefault();
+
+
+    // objet et ses paramètres qui vont être ajoutés au panier 
+    let addItem = {
+
+      id: newUrl,
+      name: titre.textContent,
+      price: prix.textContent,
+      quantity: itemQty.value,
+      color: colorSelect.value,
     };
+    console.log(addItem);
 
-  })
-  // message d'erreur
-  .catch(e => {
-    errorMessage("Veuillez passer sur le localhost:3000");
-    console.log(e);
+    // création du panier (localstorage)
+    let cart = [];
+
+    // si le localstorage contient déjà des éléments on les récupèrent, puis on insert les nouveaux 
+    if (localStorage.getItem("item") !== null) {
+      cart = JSON.parse(localStorage.getItem("item"));
+    }
+    // si il n'existe pas on crée un nouveau avec en poussant un nouvelle objet (addItem)
+    cart.push(addItem);
+    localStorage.setItem("item", JSON.stringify(cart));
+
+    // message de confirmation   
+      alert('le produit a été ajouté à votre panier')
+   
   });
 
-  
-
-
-
+};
+addToCart();
--- front/js/script.js
+++ front/js/script.js
@@ -1,21 +1,21 @@
 // appel de l'api 
 
-  fetch(`http://localhost:3000/api/products/`)
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data);
+fetch(`http://localhost:3000/api/products/`)
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data);
 
 
-      // variable pour l'affichage du html
-      let affichage = "";
+    // variable pour l'affichage du html
+    let affichage = "";
 
 
-      // boucle pour avoir les éléments du tableau 
-      for (let i = 0; i < data.length; i++) {
+    // boucle pour avoir les éléments du tableau 
+    for (let i = 0; i < data.length; i++) {
 
-        affichage += `
+      affichage += `
 
-    <a href="./product.html?${data[i]._id}">
+        <a href="./product.html?id=${data[i]._id}">
             <article>
               <img src="${data[i].imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
               <h3 class="productName">${data[i].name}</h3>
@@ -23,16 +23,11 @@
             </article>
           </a> `;
 
-      }
+    }
 
-      document.getElementById("items").innerHTML = affichage;
+    document.getElementById("items").innerHTML = affichage;
 
-    })
+  });
 
-    // message d'erreur
-    .catch(e => {
-      errorMessage("Veuillez passer sur le localhost:3000");
-      console.log(e);
-    });
 
-  
+
